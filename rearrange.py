91fd0c29f8ab5ef925840467832cd2ec488221b7
swxft <edenweb@outlook.com>
2022-01-24 12:35:22 -0500

    refactored code to fulfill all reqs.

--- rearrange.py
+++ rearrange.py
@@ -1,15 +1,12 @@
-import random
+import random, sys
 # rearrange a list of words
 
-def word_shuffler(word_list):
-    og_word_list = word_list.split()
-    print(f"the original word list is: {og_word_list}")
-    shuffled_word_list = random.sample(og_word_list, len(og_word_list))
-    return shuffled_word_list
+def word_shuffler(word_input):
+    shuffled_words = tuple(random.sample(word_input, len(word_input)))
+    return shuffled_words
 
 
 if __name__ == '__main__':
-    word_list = input("type words here separated by space only: ")
-    new_sentence = word_shuffler(word_list)
-    print(f"you wrote: {word_list}")
+    word_input = sys.argv[1:]
+    new_sentence = " ".join(word_shuffler(word_input))
     print(f"the rearranged result is: {new_sentence}.")
