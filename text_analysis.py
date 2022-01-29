1855814b23d2e1e10079380e52dd65841f84eefc
swxft <edenweb@outlook.com>
2022-01-28 22:01:06 -0500

    unique words and frequency functions fully built out
    step 3 completed fully

--- text_analysis.py
+++ text_analysis.py
@@ -1,6 +1,6 @@
-import sys, random, re
-
-def histogram(source_location, structure = "lol"):
+import sys, random, dictionary_words, timeit
+# categorize tokens by their types
+def histogram(source_location, structure = "lot"):
     with open(source_location) as lrg_string:
         words_list = lrg_string.read().split()
         words_dict = {}
@@ -12,12 +12,45 @@ def histogram(source_location, structure = "lol"):
             words_dict.update({word: 1}) 
 
     if structure == "lol":
-        print(f"list of lists: {list(map(list, words_dict.items()))}")
-    elif structure == "lot" :
-        print(f"list of tuples: {list(map(tuple, words_dict.items()))}")
+        # print(f"dictionary: {words_dict.items()}")
+        words_dict = list(map(list, words_dict.items()))
+    elif structure == "lot":
+        words_dict = list(map(tuple, words_dict.items()))
     return words_dict
+
+def unique_words(histogram_tuple):
+    # when given the histogram for The Adventures of Sherlock Holmes, it returns the integer 8475.
+    unique_words = len(histogram_tuple)
+    return unique_words
+
+def frequency(histogram_tuple_list, word = "you"):
+    # For example, when given the word "mystery" and the Holmes histogram, it will return the integer 20.
+    key_list = []
+    for pair in histogram_tuple_list:
+        key_list.append(pair[0]) # windmill
+    tuple_index = key_list.index(word)  #21
+    tuple_item = histogram_tuple_list[tuple_index]
+    word_frequency = tuple_item[-1] # 4
+    return word_frequency
 
 
 if __name__ == '__main__':
-    histogram("feels_good_inc.txt", "lol") # list of lists
-    histogram("feels_good_inc.txt", "lot") # list of tuples
+
+    # python3 text_analysis.py text_file structure frequency
+    # "lot" list of tuples and "lol" list of lists
+    list_user_args = sys.argv[1:] # list of args starting with the 2nd item
+
+    # test the histogram function
+    histogram(list_user_args[0], list_user_args[1])
+
+    # test result
+    histogram_result = histogram(list_user_args[0])
+
+    # test unique words
+    print(f"the amount of unique words is: {unique_words(histogram_result)}")
+
+    # test frequency of words
+    # print(f"frequency of your word is: {frequency(histogram_result, 'windmill')}")
+    print(f"frequency of your word is: {frequency(histogram_result, list_user_args[-1])}")
+
+    # list_test = timeit.timeit(histogram("feels_good_inc.txt", "lot"), number=1000000)
