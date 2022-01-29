b66864fa8545026b846f3add9726fc94a1d62622
swxft <edenweb@outlook.com>
2022-01-28 22:15:10 -0500

    cli input as args and accepts any file for word gen

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,10 +1,9 @@
-# from hashlib import new
 import sys, random, re
-#  generating sentences from a body of text in a text file. 
+#  generate sentences from a body of text in a file. 
 
 def random_python_quote(corpus, words_requested):
-    with open(corpus) as dictionary:
-        text_body = dictionary.read().splitlines()
+    with open(corpus) as vocabulary:
+        text_body = vocabulary.read().split()
         new_words = (random.choices(text_body, k = int(words_requested)))
         new_sentence = " ".join(new_words)
         print(f"{new_sentence}.")
@@ -13,6 +12,6 @@ def random_python_quote(corpus, words_requested):
 if __name__ == '__main__':
     # corpus_location word_amount
     list_user_args = sys.argv[1:] #list
-    words_requested = " ".join(list_user_args)
-    random_python_quote('words.txt', words_requested)
+    words_requested = " ".join(list_user_args[1])
+    random_python_quote(list_user_args[0], words_requested)
     
