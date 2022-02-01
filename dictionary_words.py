7d5e032d9bf563a474642c83a6f0f27380196632
swxft <edenweb@outlook.com>
2022-01-31 20:33:31 -0500

    checkpoint with sample function in progress

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,5 +1,5 @@
-import sys, random, re
-#  generate sentences from a body of text in a file. 
+import sys, random, text_analysis, sample
+#  generate sentences from a body of text.
 
 def random_python_quote(corpus, words_requested):
     with open(corpus) as vocabulary:
@@ -18,9 +18,33 @@ def histogram_random_word(histogram_tuple_list, words_requested = 1):
     new_sentence = " ".join(new_words)
     return new_sentence
 
+def stochastic_random(histogram_tuple_list, words_requested = 1):
+    key_list = []
+    for pair in histogram_tuple_list:
+        key_list.append(pair) # windmill
+        print(f"pair: {pair}")
+        print(f"pair: {pair[0]}")
+        # print(f"key list: {key_list}")
+    # new_words = (random.choices(key_list, k = int(words_requested)))
+    # new_sentence = " ".join(new_words)
+    # return new_sentence
+    pass
+
+def uniform_list(histogram_tuple_list, words_requested = 1):
+    # print(histogram_tuple_list)
+    token_list = []
+    for pair in histogram_tuple_list:
+        type, token = pair[0], pair[1]
+        token_list.extend([type] * token)
+    # print(len(token_list))
+    return token_list
+
 if __name__ == '__main__':
     # corpus_location word_amount
     list_user_args = sys.argv[1:] #list
-    words_requested = " ".join(list_user_args[1])
-    random_python_quote(list_user_args[0], words_requested)
+    # words_requested = " ".join(list_user_args[1])
+    # random_python_quote(list_user_args[0], words_requested)
+    histogram_tuple_list = text_analysis.histogram(list_user_args[0])
+    # print(sample.number_line(histogram_tuple_list))
+    print(f" list: {uniform_list(histogram_tuple_list)}")
     
--- sample.py
+++ sample.py
@@ -1,4 +1,6 @@
+import re, random
 import sys, random, text_analysis, dictionary_words
+from threading import get_ident
 
 def random_word_from_histogram(file, words_requested):
     histogram = text_analysis.histogram(file, words_requested) #list of tuples
@@ -9,12 +11,31 @@ def random_word_from_file(file, words_requested = 1):
     word = dictionary_words.random_python_quote(file, words_requested)
     return(word)
 
+def sample_word(histogram_tuple_list, token_list):
+    print(f"type list: {histogram_tuple_list}")
+    # random.random(0, token_amount)
+    distance = 1
+    dart = random.randint(1, len(token_list))
+    for word in histogram_tuple_list:
+        type, token = word[0], word[1]
+        type = word[0]
+        if dart >= distance:
+            distance += token
+        else:
+            random_word = type
+    return random_word
+
+def number_line(histogram_tuple):
+    token_list = []
+    for pair in histogram_tuple:
+        type, token = pair[0], pair[1]
+        token_list.extend([type] * token)
+    return token_list
 
 if __name__ == '__main__':
     list_user_args = sys.argv[1:] #list
-    # words_requested = " ".join(list_user_args[-1])
-    # corpus_location word_amount
-    print(f"word from histogram: {random_word_from_histogram(list_user_args[0],1)}")
-    # corpus_location word_amount
-    print(f"word from file: {random_word_from_file(list_user_args[0],1)}")
+
+    hish = text_analysis.histogram("onefish.txt")
+    # print(number_line(hish))
+    sample_word(hish)
     # dictionary_words.random_python_quote(list_user_args[0], words_requested[1])
